d6b7bc55ac47caaba7be8a7eb5e7899ef3914e27
kuba10155 <j.zborowski24@gmail.com>
2023-02-09 05:08:51 +0100

    Update Question.jsx

--- src/Question.jsx
+++ src/Question.jsx
@@ -1,20 +1,25 @@
 import React from "react"
+import Answer from "./Answer"
 
 export default function Question(props) {
 
-console.log(props.answers)
+  const answerElements = props.answers.map(answer => <Answer
+    key={answer.id}
+    clicked={props.clicked}
+    questionId={props.id}
+    value={answer.value}
+    id={answer.id}
+    isHeld={answer.isHeld}
+    isGoingToCheck={props.isGoingToCheck}
+    isCorrect={answer.isCorrect}
+    />)
 
   return (
       <div className="solo">
         <h3 className="question">{props.question}</h3>
         <div className="answers">
-          <button className="answer" style={{background: props.answers[0].isHeld ? "#D6DBF5" : "white"}} onClick={() => props.clicked(props.id,props.answers[0].id)}>{props.answers[0].value}</button>
-          <button className="answer" style={{background: props.answers[1].isHeld ? "#D6DBF5" : "white"}} onClick={() => props.clicked(props.id,props.answers[1].id)}>{props.answers[1].value}</button>
-          <button className="answer" style={{background: props.answers[2].isHeld ? "#D6DBF5" : "white"}} onClick={() => props.clicked(props.id,props.answers[2].id)}>{props.answers[2].value}</button>
-          <button className="answer" style={{background: props.answers[3].isHeld ? "#D6DBF5" : "white"}} onClick={() => props.clicked(props.id,props.answers[3].id)}>{props.answers[3].value}</button>
+          {answerElements}
         </div>
       </div>
   )
 }
-
-// <p>{props.difficulty}</p> <p>{props.category}</p>
